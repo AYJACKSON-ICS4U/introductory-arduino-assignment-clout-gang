b70ea93683dec062deb5637e4c89c35d5b54ce11
clementfranklin <31698115+clementfranklin@users.noreply.github.com>
2017-12-06 10:03:59 -0500

    Update LED on|off with milli delay.ino
    fix lol

--- LED on|off with milli delay.ino
+++ LED on|off with milli delay.ino
@@ -1,27 +1,47 @@
-        /*Simple_PB_LED_V1   */
-
 const byte LEDpin = 3;
 const byte SWITCHpin = 2;
-unsigned long time;
 
+int ledPin =  3;      // the number of the LED pin
+int ledState = LOW;             // ledState used to set the LED
+unsigned long previousMillis = 0;        // will store last time LED was updated
+long OnTime = 250;           // milliseconds of on-time
+long OffTime = 750;          // milliseconds of off-time
+long interval = 100;
+
+ 
 byte LEDstate = LOW;
+//byte LEDstate = HIGH;
+byte SWITCHstate;
+byte lastSWITCHstate;
 
-void setup() {
-  Serial.begin(9600);
-  // put your setup code here, to run once:
-  pinMode(LEDpin, OUTPUT);
+void setup() 
+{
+  // set the digital pin as output:
+  pinMode(ledPin, OUTPUT);      
   pinMode(SWITCHpin, INPUT);
   digitalWrite(SWITCHpin, HIGH);
+
 }
+ 
+void loop()
+{
+  // check to see if it's time to change the state of the LED
+  unsigned long currentMillis = millis();
+ 
+   SWITCHstate = digitalRead(SWITCHpin);
+ 
+  if ((lastSWITCHstate == HIGH) && (SWITCHstate == LOW))
+  {
 
+   unsigned long currentMillis = millis();
+ 
+    if(currentMillis - previousMillis > interval){
+    previousMillis = currentMillis;   
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  LEDstate = digitalRead(SWITCHpin);
-  time = millis();
-  Serial.println(time);
-   delay (100);
-  Serial.print("Time: ");
-  LEDstate=!LEDstate;
-  digitalWrite(LEDpin, LEDstate);
-} 
+      LEDstate = !LEDstate;
+      digitalWrite(LEDpin, LEDstate);
+    } 
+}
+lastSWITCHstate = SWITCHstate;
+
+}
